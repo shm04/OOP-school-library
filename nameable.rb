f1addf56e5f58b74e1187cefadebc8a56bec7812
Sebastian Hernandez <sebastian4hm@gmail.com>
2023-08-17 21:32:02 -0600

    decorate a class

---
+++ nameable.rb
@@ -0,0 +1,46 @@
+class Nameable
+    def correct_name
+      raise NotImplementedError, "Subclasses must implement this method"
+    end
+  end
+  
+  class Person < Nameable
+    def initialize(name)
+      @name = name
+    end
+  
+    def correct_name
+      @name
+    end
+  end
+  
+  class Decorator < Nameable
+    def initialize(nameable)
+      @nameable = nameable
+    end
+  
+    def correct_name
+      @nameable.correct_name
+    end
+  end
+  
+  class CapitalizeDecorator < Decorator
+    def correct_name
+      @nameable.correct_name.capitalize
+    end
+  end
+  
+  class TrimmerDecorator < Decorator
+    def correct_name
+      original_name = @nameable.correct_name
+      original_name.length > 10 ? original_name[0..9] : original_name
+    end
+  end
+  
+  person = Person.new('maximilianus')
+  person.correct_name
+  capitalized_person = CapitalizeDecorator.new(person)
+  puts capitalized_person.correct_name
+  capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+  puts capitalized_trimmed_person.correct_name
+  
